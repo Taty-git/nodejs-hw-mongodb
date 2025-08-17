d977a430164b3b0e81873495ed98e1934390f8b8
Татьяна Татьяна <taty.bystrova@gail.com>
2025-08-17 19:28:36 +0300

    rewtite server.js

--- scr/server.js
+++ scr/server.js
@@ -1,77 +1,68 @@
 import express from 'express';
+import crypto from 'node:crypto';
 import pino from 'pino-http';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import { getEnvVar } from './utils/getEnvVar';
-import { getAllStudents, getStudentById } from './services/students';
-
-dotenv.config();
-
-const PORT = Number(getEnvVar('PORT', '3000'));
+import { getEnvVar } from './utils/getEnvVar.js';
+import { ENV_VARS } from './constants/envVars.js';
+import { getStudentById, getStudents } from './service/students.js';
 
 export const startServer = () => {
   const app = express();
 
-  app.use(express.json());
-  app.use(cors());
-
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  // middleware
+  app.use([
+    (req, res, next) => {
+      req.id = crypto.randomUUID();
+      next();
+    },
+    pino(),
+    cors(),
+  ]);
 
   app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
-
-    res.status(200).json({
+    const students = await getStudents();
+    res.json({
+      status: 200,
+      message: 'Successfully found students!',
       data: students,
     });
   });
 
-   app.get('/students/:studentId', async (req, res, next) => {
+  app.get('/students/:studentId', async (req, res) => {
     const { studentId } = req.params;
-    const student = await getStudentById(studentId);   
-    
-    // Відповідь, якщо контакт не знайдено
-	if (!student) {
-	  res.status(404).json({
-		  message: 'Student not found'
-	  });
-	  return;
-	}
+    const student = await getStudentById(studentId);
 
-	// Відповідь, якщо контакт знайдено
-    res.status(200).json({
+    if (!student) {
+      return res.status(404).json({
+        status: 404,
+        message: `Student with ${studentId} not found!`,
+      });
+    }
+
+    res.json({
+      status: 200,
+      message: `Successfully found student with id ${studentId}!`,
       data: student,
     });
   });
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Route not found!',
+      status: 404,
+    });
+  });
+
+  app.use('/', (err, req, res, next) => {
+    res.status(500).json({
+      status: 500,
+      message: 'Oops error happened in application!',
+      error: err.message,
+    });
+  });
+
+  const PORT = getEnvVar(ENV_VARS.PORT, 3000);
+  app.listen(PORT, () => {
+    console.log(`Server is running in ${PORT} port!`);
+  });
 };
-
-
-	
