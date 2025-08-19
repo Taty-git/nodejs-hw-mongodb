d49aed428e560ad6f058e14ced62ab4b81ce2538
Татьяна Татьяна <taty.bystrova@gail.com>
2025-08-19 21:29:07 +0300

    rewrite and check files

--- src/middlewares/notFoundMiddleware.js
+++
@@ -1,6 +0,0 @@
-export const notFoundMiddleware = (req, res) => {
-    res.status(404).json({
-        message: 'Route not found!',
-        status: 404,
-    });
-};
--- src/middlewares/requestIdMiddleware.js
+++
@@ -1,6 +0,0 @@
-import crypto from 'node:crypto';
-
-export const requestIdMiddleware = (req, res, next) => {
-    req.id = crypto.randomUUID();
-    next();
-};
